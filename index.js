a7d26abcbf3a4710a3c37c91226421dfdea57d9e
ROCKFISH\mike.praksti <mike.praksti@rockfishinteractive.com>
2016-11-02 18:50:36 -0400

    e

--- index.js
+++ index.js
@@ -15,37 +15,7 @@ var rules = [{
 }];
 
 app.post('/rules', function(req, res) {
-    if (req.body.request.type === 'LaunchRequest') {
-        res.json({
-            "version": "1.0",
-            "response": {
-                "shouldEndSession": true,
-                "outputSpeech": {
-                    "type": "SSML",
-                    "ssml": "<speak>You insignifigant pleb, do not test my patience. <break time=\"1s\"/> What do you want to wish to divine from me?</speak>"
-                }
-            }
-        });
-    } else if (req.body.request.type === 'IntentRequest' &&
-             req.body.request.intent.name === 'Rule') {
-
-        if (!req.body.request.intent.slots.AMAZON.NUMBER ||
-            !req.body.request.intent.slots.AMAZON.NUMBER.value) {
-        }
-
-        var currRule = rules[req.body.request.intent.slots.AMAZON.NUMBER].rule;
-
-        res.json({
-            "version": "1.0",
-            "response": {
-                "shouldEndSession": true,
-                "outputSpeech": {
-                    "type": "SSML",
-                    "ssml": "<speak>currRule</speak>"
-                }
-            }
-        });
-    }
+    console.log(req);
 });
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
